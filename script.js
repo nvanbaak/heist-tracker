962787a99f2e8d1f4e6c587df55608cad6057dc5
Nikolai <nvanbaak@gmail.com>
2020-09-20 18:55:01 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function drawClock() {
+
+    var canvas = document.getElementById("test");
+    if (canvas.getContext) {
+
+        var ctx = canvas.getContext('2d');
+    
+        
+    
+    }
+
+
+
+}
