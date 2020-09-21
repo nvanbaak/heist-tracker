837f69b000593ccb5eebb7a4f15090a7c89c5c9a
Nikolai <nvanbaak@gmail.com>
2020-09-21 12:28:23 -0700

    refactored drawClock() to eliminate extraneous function

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-drawClock(2,6);
+const BLADESBLUE = "#444466";
+const BLADESGREEN = "#446644";
+const BLADESRED = "#884444";
+
+drawClock("test", 2, 6, BLADESRED);
 
 
-function drawClock(complete, segment) {
+function drawClock(canvasID, complete, segment, colorStr) {
 // Draws a clock with the specified number of segments and fills in some of them.
 
-    var canvas = document.getElementById("test");
+    var canvas = document.getElementById(canvasID);
     if (canvas.getContext) {
 
         // set up canvas context
@@ -13,7 +17,7 @@ function drawClock(complete, segment) {
         canvas.height = 200;
 
         // Draw clock background
-        ctx.fillStyle = "#444466";
+        ctx.fillStyle = colorStr;
         ctx.beginPath();
         ctx.arc(100,100,100,0,Math.PI * 2);
         ctx.fill();
@@ -21,57 +25,42 @@ function drawClock(complete, segment) {
         // Create the appropriate number of segments
         for (i = 0; i < segment; i++) {
 
-            drawClockSegment(ctx,i,segment,(i < complete))
+            ctx.strokeStyle = "#FFFFFF";
+
+            // Define segment size
+            var segSize = Math.PI * (2/segment);
+        
+            // We start at the top of the circle; each subsequent segment starts an appropriate distance along the circumference
+            var arcStart = 0 - (Math.PI * (1/2)) + (i * segSize);
+            var arcEnd = arcStart + segSize;
+        
+            // Start drawing
+            ctx.beginPath();
+            // Move pen to the middle of the canvas
+            ctx.moveTo(100,100);
+            // Draw arc centered on the canvas with endpoints as defined above
+            ctx.arc(100,100,98,arcStart,arcEnd)
+            // Draw a line back to the middle
+            ctx.lineTo(100,100);
+        
+            // Close the shape; if it's not completed yet, we fill it in grey and add an outline
+            if ( i >= complete ) {
+                ctx.fillStyle = "#aaaaaa"
+                ctx.fill();
+        
+                // If we fill, we draw an unfilled copy on top with white outline
+                ctx.strokeStyle = "#FFFFFF";
+                ctx.beginPath();
+                ctx.moveTo(100,100);
+                ctx.arc(100,100,98,arcStart,arcEnd)
+                ctx.lineTo(100,100);
+                ctx.stroke();
+        
+            } else {
+                ctx.stroke();
+            }
+
 
         }
     }
-}
-
-function drawClockSegment(ctx, segNum, segTotal, complete, colorStr) {
-    // This function draws a segment of a clock
-    // ctx        Canvas context
-    // segNum     This segment's position in the clock 
-    // segTotal   The total number of segments in this clock
-    // outline    A boolean that tells us whether the segment is complete
-    // color      A string representing a color
-
-    // Reset stroke style based on whether the segment is filled
-    ctx.strokeStyle = "#FFFFFF";
-
-    // Define segment size
-    var segSize = Math.PI * (2/segTotal);
-
-    // We start at the top of the circle; each subsequent segment starts an appropriate distance along the circumference
-    var arcStart = 0 - (Math.PI * (1/2)) + (segNum * segSize);
-    var arcEnd = arcStart + segSize;
-
-    // Start drawing
-    ctx.beginPath();
-    // Move pen to the middle of the canvas
-    ctx.moveTo(100,100);
-    // Draw arc centered on the canvas with endpoints as defined above
-    ctx.arc(100,100,98,arcStart,arcEnd)
-    // Draw a line back to the middle
-    ctx.lineTo(100,100);
-
-    // Close the shape; we only fill if it's been completed
-    if (!complete) {
-        ctx.fillStyle = "#aaaaaa"
-        ctx.fill();
-
-        // If we fill, we draw an unfilled copy on top with white outline
-        ctx.strokeStyle = "#FFFFFF";
-        ctx.beginPath();
-        ctx.moveTo(100,100);
-        ctx.arc(100,100,98,arcStart,arcEnd)
-        ctx.lineTo(100,100);
-        ctx.stroke();
-
-    } else {
-        ctx.stroke();
-    }
-
-
-
-
 }
