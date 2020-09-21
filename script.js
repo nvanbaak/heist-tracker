ac7843c8cfe17359640e2115002545b3ae9aebc3
Nikolai <nvanbaak@gmail.com>
2020-09-20 19:37:07 -0700

    drawClock can draw circle segments now

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+drawClock();
 
 function drawClock() {
 
@@ -6,8 +6,29 @@ function drawClock() {
     if (canvas.getContext) {
 
         var ctx = canvas.getContext('2d');
-    
+
+        canvas.width = 200;
+        canvas.height = 200;
+
+        var arcStart = 0 - (Math.PI * (1/2));
+        var arcEnd = arcStart + (Math.PI * (1/4));
+
         
+
+        // Start drawing
+        ctx.beginPath();
+
+        // Move pen to the middle of the canvas
+        ctx.moveTo(100,100);
+
+        // Draw arc centered on the canvas
+        ctx.arc(100,100,98,arcStart,arcEnd, false)
+
+        // Draw a line back to the middle
+        ctx.lineTo(100,100);
+
+        // Close the shape
+        ctx.stroke();
     
     }
 
